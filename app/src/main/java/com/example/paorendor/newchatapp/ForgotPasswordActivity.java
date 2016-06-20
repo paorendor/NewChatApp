e515e3419c256211322cab5c4cf97b8458641f89
Pao Rendor <PaoRendor@Airs-MacBook-Air.local>
2016-06-20 21:02:43 +0800

    created forgot password, contacts, messages

--- app/src/main/java/com/example/paorendor/newchatapp/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/paorendor/newchatapp/ForgotPasswordActivity.java
@@ -2,6 +2,8 @@ package com.example.paorendor.newchatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
